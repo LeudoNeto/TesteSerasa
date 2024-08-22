396f835b7c50384754527fdc9f8e6065cdf6b325
Leudo Neto <leudoneto1234@gmail.com>
2024-08-22 18:06:29 -0300

    feat/validação de cpf, cnpj, áreas

--- api/produtor_rural/serializers.py
+++ api/produtor_rural/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import ProdutorRural, Cultura
+from .utils import valida_cpf, valida_cnpj
 
 class CulturaSerializer(serializers.ModelSerializer):
     class Meta:
@@ -14,3 +15,14 @@ class ProdutorRuralSerializer(serializers.ModelSerializer):
             'cidade', 'estado', 'area_total_hectares', 
             'area_agricultavel_hectares', 'area_vegetacao_hectares',
         ]
+
+    def validate(self, data):
+        if data['usa_cpf'] and not valida_cpf(data['cpf_cnpj']):
+            raise serializers.ValidationError("CPF inválido.")
+
+        if not data['usa_cpf'] and not valida_cnpj(data['cpf_cnpj']):
+            raise serializers.ValidationError("CNPJ inválido")
+
+        if data['area_agricultavel_hectares'] + data['area_vegetacao_hectares'] > data['area_total_hectares']:
+            raise serializers.ValidationError("A soma da área agricultável e da vegetação não pode ser maior que a área total da fazenda.")
+        return data
---
+++ api/produtor_rural/utils.py
@@ -0,0 +1,36 @@
+def valida_cpf(cpf: str) -> bool:
+    if len(cpf) != 14:
+        return False
+    cpf = cpf.replace('.', '').replace('-', '')
+    if len(cpf) != 11 or not cpf.isdigit():
+        return False
+
+    def calcular_digito(cpf: str, posicoes: int) -> int:
+        soma = sum(int(cpf[i]) * (posicoes - i) for i in range(posicoes - 1))
+        resto = (soma * 10) % 11
+        return 0 if resto == 10 else resto
+
+    digito1 = calcular_digito(cpf, 10)
+    digito2 = calcular_digito(cpf, 11)
+
+    return cpf[-2:] == f'{digito1}{digito2}'
+
+def valida_cnpj(cnpj: str) -> bool:
+    if len(cnpj) != 18:
+        return False
+    cnpj = cnpj.replace('.', '').replace('/', '').replace('-', '')
+    if len(cnpj) != 14 or not cnpj.isdigit():
+        return False
+
+    def calcular_digito(cnpj: str, pesos: list) -> int:
+        soma = sum(int(cnpj[i]) * peso for i, peso in enumerate(pesos))
+        resto = (soma % 11)
+        return 0 if resto < 2 else 11 - resto
+
+    pesos1 = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
+    pesos2 = [6] + pesos1
+
+    digito1 = calcular_digito(cnpj, pesos1)
+    digito2 = calcular_digito(cnpj + str(digito1), pesos2)
+
+    return cnpj[-2:] == f'{digito1}{digito2}'
