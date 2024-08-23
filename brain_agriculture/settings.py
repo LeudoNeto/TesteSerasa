67b3bccec7174fd384dfd06353f6726e49001720
Leudo Neto <leudoneto1234@gmail.com>
2024-08-23 11:44:54 -0300

    feat/static root

--- brain_agriculture/settings.py
+++ brain_agriculture/settings.py
@@ -144,6 +144,9 @@ STATICFILES_DIRS = [
     'frontend/assets/'
 ]
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
